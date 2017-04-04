3eb7ca82ad984ade098c33220792b9736cf6d225
Jason Masurovsky <jason.masurovsky@trincoll.edu>
2017-04-04 18:24:09 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/17EulkVCj79TNsUKxxT_FawdonT_mfgKnWU1M2G89NsY/edit#gid=164271551';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1YLLly5pjE5Q-qgCJXhq5Cj4C0oaHBcjXASYcpLvcs48/edit#gid=0';
